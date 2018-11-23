89a83f1eef0394b26d5925ee8164ada2adc80b5e
Li Yun-Lung <obigbando@gmail.com>
2018-11-23 12:51:38 +0800

    add invFlag to AtgxReadsMultipleNFilter

--- adam-cli/src/main/scala/org/bdgenomics/adam/cli/AtgxReadsMultipleNFilter.scala
+++ adam-cli/src/main/scala/org/bdgenomics/adam/cli/AtgxReadsMultipleNFilter.scala
@@ -3,13 +3,19 @@ package org.bdgenomics.adam.cli
 import org.bdgenomics.formats.avro.AlignmentRecord
 
 class AtgxReadsMultipleNFilter {
-  def filterN(iter: Iterator[AlignmentRecord], maxN: Int): Iterator[AlignmentRecord] = {
+  def filterN(iter: Iterator[AlignmentRecord], maxN: Int, invFlag: Boolean): Iterator[AlignmentRecord] = {
     iter.filter { record =>
       val nCount = record.getSequence.filter(_ == 'N').length
-      if (nCount > maxN)
-        false
-      else
+
+      if (!invFlag)
+        if (nCount > maxN)
+          false
+        else
+          true
+      else if (nCount > maxN)
         true
+      else
+        false
     }
   }
 }
