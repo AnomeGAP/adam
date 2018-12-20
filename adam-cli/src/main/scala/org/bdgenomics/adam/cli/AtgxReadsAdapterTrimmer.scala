d17beb28795ae93ef2b90af5afe794c236845bee
Sid <sid666dmz@gmail.com>
2018-12-20 15:14:50 +0800

    adapter trimmer without prepare pairing

--- adam-cli/src/main/scala/org/bdgenomics/adam/cli/AtgxReadsAdapterTrimmer.scala
+++ adam-cli/src/main/scala/org/bdgenomics/adam/cli/AtgxReadsAdapterTrimmer.scala
@@ -3,64 +3,61 @@ package org.bdgenomics.adam.cli
 import org.bdgenomics.formats.avro.AlignmentRecord
 import org.bdgenomics.adam.cli.Utils.reverseComplementary
 
-import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.{HashMap, ListBuffer}
 
 class AtgxReadsAdapterTrimmer {
   def trim(iter: Iterator[AlignmentRecord]): Iterator[AlignmentRecord] = {
-    val (it1, it2) = iter.duplicate
-    val (read1, read2) = it1.partition { record =>
+    val unpairedReads = new HashMap[Long, AlignmentRecord]()
+    iter.flatMap { record =>
       val name = record.getReadName
       val (_, iw) = AtgxReadsInfoParser.parseFromName(name)
-      // use readId to identify read1 read2
-      if ((iw.getID & 0x1) == 0)
-        true
-      else
-        false
-    }
-    // use the position of A to represent the sequence
-    // the reason to use A is A has higher precision in Illumina 2-color chemistry
-    val read1APos = read1.map { record =>
-      val seq = record.getSequence
-      val firstAToHeadDist = seq.indexOf('A')
+      val readId = iw.getID
+      val anotherPairId = if ((readId & 0x1) == 0) readId + 1 else readId - 1
 
-      if (firstAToHeadDist == -1) {
-        None
-      } else {
-        Some(calcRelativeDist(seq, 'A') -> firstAToHeadDist)
-      }
-    }
+      unpairedReads.get(anotherPairId)
+        .map { another =>
+          unpairedReads.remove(anotherPairId)
+          // use the position of A to represent the sequence
+          // the reason to use A is A has higher precision in Illumina 2-color chemistry
+          val read1APos = getRead1APos(record)
+          val read2APos = getRead2APos(another)
+          val overlapLen = (read1APos, read2APos) match {
+            case (Some(r1APos), Some(r2APos)) => {
+              val common = longestCommonSubLists(r1APos._1, r2APos._1)
+              Some(r1APos._2 + common.sum + r2APos._2)
+            }
+            case _ => None
+          }
 
-    val read2APos = read2.map { record =>
-      val rcSeq = reverseComplementary(record.getSequence)
-      val lastIdxOfA = rcSeq.lastIndexOf('A')
-      if (lastIdxOfA == -1) {
-        None
-      } else {
-        val lastAToEndDist = rcSeq.length - lastIdxOfA - 1
-        Some(calcRelativeDist(rcSeq, 'A') -> lastAToEndDist)
-      }
-    }
-
-    val (overlapLen1, overlapLen2) = (read1APos zip read2APos map {
-      case (Some(r1APos), Some(r2APos)) => {
-        val common = longestCommonSubLists(r1APos._1, r2APos._1)
-        r1APos._2 + common.sum + r2APos._2
-      }
-      case _ => 0
-    }).duplicate
-
-    it2 zip (overlapLen1 ++ overlapLen2) map {
-      case (record, len) =>
-        if (len == 0) {
-          record
-        } else {
-          val newSeq = new String(record.getSequence.substring(0, len))
-          val newQuality = new String(record.getQual.substring(0, len))
-          record.setSequence(newSeq)
-          record.setQual(newQuality)
-
-          record
+          overlapLen.map { len => List(trimAdapter(record, len), trimAdapter(another, len)) }
+            .getOrElse { List(record, another) }
         }
+        .getOrElse {
+          unpairedReads.put(readId, record)
+          List.empty
+        } ++ unpairedReads.values
+    }
+  }
+
+  private def getRead1APos(record: AlignmentRecord): Option[(List[Int], Int)] = {
+    val seq = record.getSequence
+    val firstAToHeadDist = seq.indexOf('A')
+
+    if (firstAToHeadDist == -1) {
+      None
+    } else {
+      Some(calcRelativeDist(seq, 'A') -> firstAToHeadDist)
+    }
+  }
+
+  private def getRead2APos(record: AlignmentRecord): Option[(List[Int], Int)] = {
+    val rcSeq = reverseComplementary(record.getSequence)
+    val lastIdxOfA = rcSeq.lastIndexOf('A')
+    if (lastIdxOfA == -1) {
+      None
+    } else {
+      val lastAToEndDist = rcSeq.length - lastIdxOfA - 1
+      Some(calcRelativeDist(rcSeq, 'A') -> lastAToEndDist)
     }
   }
 
@@ -86,5 +83,14 @@ class AtgxReadsAdapterTrimmer {
     val ls2Tails = ls2.tails.toSet
 
     ls1Inits.intersect(ls2Tails).maxBy(_.length)
+  }
+
+  private def trimAdapter(record: AlignmentRecord, len: Int): AlignmentRecord = {
+    val newSeq = new String(record.getSequence.substring(0, len))
+    val newQuality = new String(record.getQual.substring(0, len))
+    record.setSequence(newSeq)
+    record.setQual(newQuality)
+
+    record
   }
 }
