8d416f8e069e513fb7426e808e32d1b71b4cc2c2
Sid <sid666dmz@gmail.com>
2019-05-31 15:37:29 +0800

    modify qual/depth encoding

--- adam-cli/src/main/scala/org/bdgenomics/adam/cli/AtgxReadsDupCollapse.scala
+++ adam-cli/src/main/scala/org/bdgenomics/adam/cli/AtgxReadsDupCollapse.scala
@@ -7,63 +7,11 @@ import scala.collection.mutable.ArrayBuffer
 
 class AtgxReadsDupCollapse extends java.io.Serializable {
   val qLevel: Array[Int] = Array[Int](
-    33, 33, 33, 33, 33, 33, 33,
-    40, 40, 40, 40, 40, 40, 40,
-    47, 47, 47, 47, 47, 47, 47,
-    54, 54, 54, 54, 54, 54, 54,
-    61, 61, 61, 61, 61, 61, 61,
-    68, 68, 68, 68, 68, 68, 68)
-
-  val dLevel: Array[Int] = Array[Int](
-    0, 0, 0, 0, 0,
-
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5
-  )
-
-  val inverseDLevel: Array[Int] = Array[Int](
-    0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2,
-    3, 3, 3, 3, 3, 3,
-    4, 4, 4, 4, 4, 4
-  )
-
+    33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33, 33,
+    47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47,
+    61, 61, 61, 61, 61, 61, 61, 61, 61, 61, 61, 61, 61, 61)
+  val depthEncoder = (d: Int) => if (d >= 14) 14 else d
   val qualMin = 33
-
   val qualMax = 74
 
   def collapse(rdd: RDD[AlignmentRecord]): RDD[AlignmentRecord] = {
@@ -79,47 +27,27 @@ class AtgxReadsDupCollapse extends java.io.Serializable {
         })
       .keyBy(x => x._3.getSequence)
       .aggregateByKey(List.empty[(Boolean, Long, AlignmentRecord)])({ case (r, c) => c :: r }, { case (r, c) => c ::: r })
-      .map(minByWithCount)
-      .flatMap(
-        x => {
-          if (!x._1)
-            Some(encodeQual(x._2, x._3))
-          else
-            None
-        })
-  }
-
-  def minByWithCount(x: (String, List[(Boolean, Long, AlignmentRecord)])): (Boolean, AlignmentRecord, Int) = {
-    var min = Long.MaxValue
-    var count = 0
-    var index = 0
-    for (i <- x._2.indices) {
-      if (x._2(i)._2 < min) {
-        min = x._2(i)._2
-        index = i
+      .filter { case (_, list) => !list.minBy(_._2)._1 } // keep list that alignment record having min ID is not RC
+      .map {
+        case (_, list) =>
+          val depth = list.size
+          val encodedDepth = depthEncoder(depth)
+          val qual = list.map(_._3.getQual.toList)
+          // get the best quality for each position in read
+          val encodedQual = qual.transpose
+            .map { i =>
+              val max = i.max
+              if (max.toInt > qualMax || max.toInt < qualMin)
+                throw new Exception("given quality does not follow Illumina 1.8+ Phred+33 quality score format")
+              max
+            }
+            // encode the quality and add depth info
+            .map(i => (qLevel(i - qualMin) + encodedDepth).toChar)
+            .mkString
+          val min = list.minBy(_._2)._3
+          min.setQual(encodedQual)
+          min
       }
-      count += 1
-    }
-    (x._2(index)._1, x._2(index)._3, count)
-  }
-
-  // convert Illumina 1.8+ Phred+33 quality score, with value range of 33-74, to depth encoded value
-  def encodeQual(item: AlignmentRecord, depth: Int): AlignmentRecord = {
-    item.setQual(
-      item.getQual
-        .map(
-          x => {
-            if (x.toInt > qualMax || x.toInt < qualMin)
-              throw new Exception("given quality does not follow Illumina 1.8+ Phred+33 quality score format")
-            (qLevel(x.toInt - qualMin) + dLevel(math.min(depth, 314))).toChar
-          })
-    )
-    item
-  }
-
-  def decodeQual(item: AlignmentRecord): (String, Array[Int]) = {
-    (item.getQual.map { x => qLevel(x.toInt - qualMin).toChar },
-      item.getQual.map { x => if (x > inverseDLevel.length) 5 else inverseDLevel(x.toInt - qualMin) }.toArray)
   }
 }
 
